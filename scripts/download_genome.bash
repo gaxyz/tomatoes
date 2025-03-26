7a55bbaefcfb2f8c8b3fe0c5b10fb51667456bba
Gaston  RIJO DE LEON <gaston.rijo-de-leon@pasteur.fr>
2025-03-26 13:05:15 +0100

    updated and restructured

---
+++ scripts/download_genome.bash
@@ -0,0 +1,2 @@
+wget -P ../data/reference_genome https://solgenomics.net/ftp/genomes/tomato13/SV_Landscape_Long_Read_Assemblies/Floradade_MAS1.0.fasta.gz
+wget -P ../data/reference_genome https://solgenomics.net/ftp/genomes/tomato13/SV_Landscape_Long_Read_Assemblies/Floradade.1.2_gene_models.gff.gz
---
+++ scripts/download_rna_heat.bash
@@ -0,0 +1,2 @@
+module load sra-tools
+fasterq-dump --gzip --outdir ../data/rna_seq/ --split-files SRX9524562
