8c441c584c0ee6c254c06a74943c51be17049003
Gaston  RIJO DE LEON <gaston.rijo-de-leon@pasteur.fr>
2025-03-26 13:06:24 +0100

    move

--- download_genome.bash
+++
@@ -1,2 +0,0 @@
-wget https://solgenomics.net/ftp/genomes/tomato13/SV_Landscape_Long_Read_Assemblies/Floradade_MAS1.0.fasta.gz
-wget https://solgenomics.net/ftp/genomes/tomato13/SV_Landscape_Long_Read_Assemblies/Floradade.1.2_gene_models.gff.gz
